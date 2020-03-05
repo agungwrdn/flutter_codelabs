2df8a8aa6871fb802c5a23b847ed438258ede5f0
Bangrawon <agungwar00@gmail.com>
2020-03-05 13:06:03 +0700

    random texxt

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,9 +14,22 @@ class MyApp extends StatelessWidget {
           title: Text('Hallo'),
         ),
         body: Center(
-          child: Text('Hai!!'),
+          child: RandomWords(),
         ),
       )
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
